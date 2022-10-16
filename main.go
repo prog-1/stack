b824fa098444b9965c03f8a85feb945f35a2dbf8
Richard <richardsasalevic@gmail.com>
2022-10-16 19:56:06 +0300

    simplified directoryes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func atoi(str string) int {
@@ -33,6 +34,26 @@ func rpn(expr []string) int {
 	return stack[0]
 }
 
+func simplify(dir string) string {
+	a := strings.Split(dir, "/")
+	var path []string
+	for _, v := range a {
+		switch v {
+		case ".":
+			continue
+		case "":
+			continue
+		case "..":
+			if len(path) != 0 {
+				path = path[:len(path)-1]
+			}
+		default:
+			path = append(path, v)
+		}
+	}
+	return "/" + strings.Join(path, "/")
+}
+
 func main() {
-	fmt.Println(rpn([]string{"3", "4", "+", "2", "*"}))
+	fmt.Println(simplify("/foo/./bar/../../baz/"))
 }
