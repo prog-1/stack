11063a18abfd654cdca402f6d91c99cec79f880f
Aleksejs Pjavka <apjavka07@gmail.com>
2022-10-30 16:49:16 +0200

    exercise 02 done

---
+++ ex02/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestPath(t *testing.T) {
+	for _, tc := range []struct {
+		n    string
+		want string
+	}{
+
+		{"/home/", "/home"},
+		{"/../", "/"},
+		{"/foo/./bar/../../baz/", "/baz"},
+		{"/foo/./bar/../../baz/boo/", "/baz/boo"},
+		{"/foo/./bar/baz/boo/../../../../../../", "/"},
+		{"/foo/./bar/../../baz/././././../boo/", "/boo"},
+	} {
+		if got := simplify(tc.n); got != tc.want {
+			t.Errorf("path(%v) = %v, want = %v", tc.n, got, tc.want)
+		}
+	}
+}
